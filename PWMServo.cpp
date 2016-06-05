ac5b6cef6a4ae63930cb6a745e8190380723ca2f
PaulStoffregen <paul@pjrc.com>
2016-06-04 17:51:58 -0700

    Support for 32 bit Teensy boards

--- PWMServo.cpp
+++ PWMServo.cpp
@@ -1,9 +1,4 @@
-#include <avr/interrupt.h>
-#if defined(ARDUINO) && ARDUINO >= 100
 #include "Arduino.h"
-#else
-#include "wiring.h"
-#endif
 #include "PWMServo.h"
 
 /*
@@ -29,6 +24,11 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+#define NO_ANGLE (0xff)
+
+#if defined(__AVR__)
+
+#include <avr/interrupt.h>
 
 uint8_t PWMServo::attachedA = 0;
 uint8_t PWMServo::attachedB = 0;
@@ -61,14 +61,7 @@ void PWMServo::seizeTimer1()
 
 void PWMServo::releaseTimer1() {}
 
-#define NO_ANGLE (0xff)
-
 PWMServo::PWMServo() : pin(0), angle(NO_ANGLE) {}
-
-uint8_t PWMServo::attach(int pinArg)
-{
-  return attach(pinArg, 544, 2400);
-}
 
 uint8_t PWMServo::attach(int pinArg, int min, int max)
 {
@@ -160,11 +153,6 @@ void PWMServo::write(int angleArg)
   #endif
 }
 
-uint8_t PWMServo::read()
-{
-  return angle;
-}
-
 uint8_t PWMServo::attached()
 {
   if (pin == SERVO_PIN_A && attachedA) return 1;
@@ -174,3 +162,53 @@ uint8_t PWMServo::attached()
   #endif
   return 0;
 }
+
+
+#elif defined(__arm__) && defined(TEENSYDUINO)
+
+uint32_t PWMServo::attachedpins[(NUM_DIGITAL_PINS+31)/32]; // 1 bit per digital pin
+
+PWMServo::PWMServo() : pin(255), angle(NO_ANGLE) {}
+
+uint8_t PWMServo::attach(int pinArg, int min, int max)
+{
+	//Serial.printf("attach, pin=%d, min=%d, max=%d\n", pinArg, min, max);
+	if (pinArg < 0 || pinArg >= NUM_DIGITAL_PINS) return 0;
+	if (!digitalPinHasPWM(pinArg)) return 0;
+	pin = pinArg;
+	analogWriteFrequency(pin, 50);
+	min16 = min >> 4;
+	max16 = max >> 4;
+	angle = NO_ANGLE;
+	digitalWrite(pin, LOW);
+	pinMode(pin, OUTPUT);
+	attachedpins[pin >> 5] |= (1 << (pin & 31));
+	return 1;
+}
+
+void PWMServo::write(int angleArg)
+{
+	//Serial.printf("write, pin=%d, angle=%d\n", pin, angleArg);
+	if (pin >= NUM_DIGITAL_PINS) return;
+	if (angleArg < 0) angleArg = 0;
+	if (angleArg > 180) angleArg = 180;
+	angle = angleArg;
+	uint32_t us = (((max16 - min16) * 46603 * angle) >> 11) + (min16 << 12); // us*256
+	uint32_t duty = (us * 3355) >> 22;
+	//float usec = (float)((max16 - min16)<<4) * ((float)angle / 180.0f) + (float)(min16<<4);
+	//uint32_t duty = (int)(usec / 20000.0f * 4096.0f);
+	//Serial.printf("angle=%d, usec=%.2f, us=%.2f, duty=%d, min=%d, max=%d\n",
+		//angle, usec, (float)us / 256.0f, duty, min16<<4, max16<<4);
+	analogWriteResolution(12);
+	analogWrite(pin, duty);
+}
+
+uint8_t PWMServo::attached()
+{
+	if (pin >= NUM_DIGITAL_PINS) return 0;
+	return (attachedpins[pin >> 5] & (1 << (pin & 31))) ? 1 : 0;
+}
+
+#endif
+
+
--- PWMServo.h
+++ PWMServo.h
@@ -48,6 +48,11 @@
   #define SERVO_PIN_A 25
   #define SERVO_PIN_B 26
   #define SERVO_PIN_C 27
+#elif defined(__arm__) && defined(TEENSYDUINO)
+  #define SERVO_PIN_A 9
+  #define SERVO_PIN_B 10
+  // all PWM pins supported, but names not defined here...
+  // just use the actual PWM pin number with attach()
 #else
   #define SERVO_PIN_A 9
   #define SERVO_PIN_B 10
@@ -60,6 +65,7 @@ class PWMServo
     uint8_t angle;       // in degrees
     uint8_t min16;       // minimum pulse, 16uS units  (default is 34)
     uint8_t max16;       // maximum pulse, 16uS units, 0-4ms range (default is 150)
+#if defined(__AVR__)
     static void seizeTimer1();
     static void releaseTimer1();
     static uint8_t attachedA;
@@ -67,18 +73,21 @@ class PWMServo
     #ifdef SERVO_PIN_C
     static uint8_t attachedC;
     #endif
+#elif defined(__arm__) && defined(TEENSYDUINO)
+    static uint32_t attachedpins[]; // 1 bit per digital pin
+#endif
   public:
     PWMServo();
-    uint8_t attach(int);
+    uint8_t attach(int pinArg) { return attach(pinArg, 544, 2400); }
                              // pulse length for 0 degrees in microseconds, 544uS default
                              // pulse length for 180 degrees in microseconds, 2400uS default
-    uint8_t attach(int, int, int);
+    uint8_t attach(int pinArg, int min, int max);
                              // attach to a pin, sets pinMode, returns 0 on failure, won't
                              // position the servo until a subsequent write() happens
                              // Only works for 9 and 10.
     void detach();
-    void write(int);         // specify the angle in degrees, 0 to 180
-    uint8_t read();
+    void write(int angleArg); // specify the angle in degrees, 0 to 180
+    uint8_t read() { return angle; }
     uint8_t attached();
 };
 
