86e99b16a340416e9f0bfa5b2c00323e56550c1d
Mikael Tulldahl <mikael.tulldahl@lavafoil.com>
2022-08-08 18:26:43 +0200

    Configurable min/max angle, dutycycle and frequency
    also supporting different types of angle now: int, float, double
    Tested on Teensy 4.1

--- examples/Sweep/Sweep.ino
+++ examples/Sweep/Sweep.ino
@@ -15,23 +15,22 @@
 
 #include <PWMServo.h>
 
-PWMServo myservo;  // create servo object to control a servo
+PWMServo<float> myservo(23);                // create servo object to control a servo
 
-int pos = 0;    // variable to store the servo position
+float pos = 0;    // variable to store the servo position
 
 void setup() {
-  myservo.attach(SERVO_PIN_A);  // attaches the servo on pin 9 to the servo object
-  //myservo.attach(SERVO_PIN_A, 1000, 2000); // some motors need min/max setting
+  myservo.attach();  // attaches the servo on pin 23 to the servo object
 }
 
 
 void loop() {
-  for(pos = 0; pos < 180; pos += 1) { // goes from 0 degrees to 180 degrees, 1 degree steps
+  for(pos = myservo.kAngleMin; pos < myservo.kAngleMax; pos += 0.1) { // goes from 0 degrees to 180 degrees, 0.1 degree steps
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+    delay(2);                       // waits 2ms for the servo to reach the position
   }
-  for(pos = 180; pos>=1; pos-=1) {   // goes from 180 degrees to 0 degrees
+  for(pos = myservo.kAngleMax; pos > myservo.kAngleMin; pos-=0.1) {   // goes from 180 degrees to 0 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+    delay(2);                       // waits 2ms for the servo to reach the position
   }
 }
